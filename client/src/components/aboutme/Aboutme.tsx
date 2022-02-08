229e11bcc11c7218c5d2ce9734efe78a68e08329
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-08 17:32:01 +0100

    add first half of about me

--- client/src/components/aboutme/Aboutme.tsx
+++ client/src/components/aboutme/Aboutme.tsx
@@ -1,19 +1,58 @@
 import "./aboutme.scss";
 import { Down } from "../intro/Down";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function AboutMe() {
     return (
         <div className="aboutme" id="aboutme">
-            <h1>ABOUT ME: coming soon!</h1>
+            <h1 className="title">About me</h1>
+            <div className="body-aboutme">
+                <div className="left">
+                    <div className="text-aboutme">
+                        <p>
+                            From the first moment I saw a "Hello World" in the
+                            browser I took the decision of mastering this magic
+                            and I joined a bootcamp. Always trying to follow the
+                            DRY principle and write clean and reusable code.
+                            With my experience as a teacher and musician I learn
+                            how to explain complex terms in a simple way and
+                            find solutions with creativity.
+                        </p>
+                    </div>
+                    <div className="icons">
+                        <a
+                            href="https://github.com/gabrielmoris"
+                            target="_blank"
+                            className="github"
+                        >
+                            <FontAwesomeIcon
+                                icon={faGithubSquare}
+                            ></FontAwesomeIcon>
+                        </a>
 
-            <iframe
-                src="https://giphy.com/embed/JIX9t2j0ZTN9S"
-                width="480"
-                height="480"
-                frameBorder="0"
-                className="giphy-embed"
-                allowFullScreen
-            ></iframe>
+                        <a
+                            href="https://www.linkedin.com/in/gabrielcmoris/"
+                            target="_blank"
+                            className="linkedin"
+                        >
+                            <FontAwesomeIcon
+                                icon={faLinkedin}
+                            ></FontAwesomeIcon>
+                        </a>
+
+                        <a href="#contact" className="envelope">
+                            <FontAwesomeIcon
+                                icon={faEnvelopeSquare}
+                            ></FontAwesomeIcon>
+                        </a>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="resume"></div>
+                </div>
+            </div>
             <a className="arrow-opinions" href="#opinions">
                 <Down />
             </a>
