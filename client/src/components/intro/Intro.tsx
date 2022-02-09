91188aa926ca083f26230e2feecb2915fc18bcd0
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-09 21:43:24 +0100

    change scss intro, topbar,menu, responsiveness and desktop

--- client/src/components/intro/Intro.tsx
+++ client/src/components/intro/Intro.tsx
@@ -19,56 +19,65 @@ export default function Intro() {
 
     return (
         <div className="intro" id="intro">
-            <div className="left">
-                <div className="text-over-img">
-                    <motion.div
-                        initial={{ x: 500 }}
-                        animate={{ x: 150 }}
-                        transition={{
-                            type: "spring",
-                            duration: 5,
-                        }}
-                    >
-                        <h1>I'm Gabriel,</h1>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: -500 }}
-                        animate={{ x: 40 }}
-                        transition={{
-                            type: "spring",
-                            duration: 5,
-                        }}
-                    >
-                        <h1>a frontend developer.</h1>
-                    </motion.div>
-                </div>
+            <div className="text-over-img">
                 <motion.div
-                    className="img-container"
-                    animate={{ x: -155 }}
-                    transition={{ type: "spring", bounce: 0.5, duration: 2 }}
+                    initial={{ x: 1000 }}
+                    // animate={{ x: 150 }}
+                    animate={{ x: "24%" }}
+                    transition={{
+                        type: "spring",
+                        duration: 5,
+                    }}
                 >
-                    <img src="./assets/yo.png" alt="Gabriel C. Moris" />
+                    <h1 className="im-gabriel">I'm Gabriel,</h1>
+                </motion.div>
+                <motion.div
+                    initial={{ x: -500 }}
+                    // animate={{ x: 40 }}
+                    animate={{ x: "18%" }}
+                    transition={{
+                        type: "spring",
+                        duration: 5,
+                    }}
+                >
+                    <h1>a frontend developer.</h1>
                 </motion.div>
             </div>
+            <div className="body-intro">
+                <div className="left">
+                    <motion.div
+                        className="img-container"
+                        // animate={{ x: -155 }}
+                        initial={{ x: -200 }}
+                        animate={{ x: -5 }}
+                        transition={{
+                            type: "spring",
+                            bounce: 0.5,
+                            duration: 2,
+                        }}
+                    >
+                        <img src="./assets/yo.png" alt="Gabriel C. Moris" />
+                    </motion.div>
+                </div>
 
-            <div className="right">
-                <div className="wrapper">
-                    <div className="text-wrapper">
-                        {" "}
-                        <h2>
-                            “An investment in knowledge always pays the best
-                            interest.”
-                            <br />
-                        </h2>
-                        {/* <h3 ref={textRef}> */}
-                        <h3>
-                            ― Andrew Hunt, The Pragmatic Programmer: From
-                            Journeyman to Master
-                        </h3>
+                <div className="right">
+                    <div className="wrapper">
+                        <div className="text-wrapper">
+                            <h2>
+                                “An investment in knowledge always pays the best
+                                interest.”
+                                <br />
+                            </h2>
+                            {/* <h3 ref={textRef}> */}
+                            <h3>
+                                ― Andrew Hunt, The Pragmatic Programmer: From
+                                Journeyman to Master
+                            </h3>
+                        </div>
+                        <a href="#works">
+                            <Down />
+                        </a>
                     </div>
-                    <a href="#works">
-                        <Down />
-                    </a>
                 </div>
             </div>
         </div>
