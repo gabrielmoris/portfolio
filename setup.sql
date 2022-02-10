88c487f77d3ecb46902ead192fe28e33dba4b721
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-10 11:44:29 +0100

    add more info and github links

--- setup.sql
+++ setup.sql
@@ -5,6 +5,7 @@
       id SERIAL PRIMARY KEY,
       img VARCHAR,
       url VARCHAR,
+      github VARCHAR,
       name TEXT,
       description TEXT,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -16,42 +17,48 @@
     points INT
   );
 
-  INSERT INTO works(img, url, name, description) VALUES (
+  INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/writtersbook.png',
     'https://writersbook.herokuapp.com/',
+    'https://github.com/gabrielmoris/writersbook',
     'WritersBook',
     'React, Redux, socket.io, Jest, AWS, PostgreSql,   
   Typescript.'
 );
-  INSERT INTO works(img, url, name, description) VALUES (
+  INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/imageboard.png',
     'https://gabrielimageboard.herokuapp.com/',
+    'https://github.com/gabrielmoris/imageboard',
     'My Imageboard',
     'Html, CSS, Javascript,Node.js, Express, Vue.js, AWS,   
   PostgreSql.'
 );
-  INSERT INTO works(img, url, name, description) VALUES (
+  INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/signaturesbank.png',
     'https://signaturebank.herokuapp.com/',
+    'https://github.com/gabrielmoris/signaturesBank',
     'Signatures Bank',
     'Html, CSS, Javascript, Node.js, Express, PostgreSql.'
 );
- INSERT INTO works(img, url, name, description) VALUES (
+ INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/connect4.png',
     'https://jovial-mestorf-9b8260.netlify.app/',
+    'https://github.com/gabrielmoris/codingChallenges/tree/master/week3/connect4',
     'Connect 4',
     'Html, CSS, Javascript, jQuery.'
 );
-  INSERT INTO works(img, url, name, description) VALUES (
+  INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/simongame.png',
     'https://clever-hoover-4bef8a.netlify.app/',
+    'https://github.com/gabrielmoris/PersonalLearning/tree/master/Simon%20Game%20JS',
     'Simon Game',
     'Html, CSS, Javascript, jQuery.'
 );
-INSERT INTO works(img, url, name, description) VALUES (
+INSERT INTO works(img, url, github, name, description) VALUES (
     'https://onionimageboard.s3.amazonaws.com/brasscollective.png',
     'https://www.thebrasscollective.com/',
     'The Brass Collective Website',
+    'https://github.com/gabrielmoris/www.thebrasscollective.com',
     'Html, CSS, Javascript, Github, Figma.'
 );
 
