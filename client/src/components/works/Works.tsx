87c53920a682566512bc595dad34f68dbd788dab
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-05-07 12:59:04 +0200

    intro style and works doesnt fetch to the database anymore

--- client/src/components/works/Works.tsx
+++ client/src/components/works/Works.tsx
@@ -1,24 +1,53 @@
-import { useEffect, useState } from "react";
 import "./works.scss";
 import WorksList from "./worksList/WorksList";
 import { Down } from "../intro/Down";
 
 export default function Works() {
-    const [works, setWorks] = useState([]);
 
-    useEffect(() => {
-        let abort = false;
-        fetch(`/works`)
-            .then((res) => res.json())
-            .then((work) => {
-                if (!abort) {
-                    setWorks(work);
-                }
-            });
-        return () => {
-            abort = true;
-        };
-    }, []);
+    const works2 = [
+        {
+            id: 1,
+            img: "./assets/workPics/writtersbook.jpg",
+            url: "https://writersbook.herokuapp.com/",
+            github: "https://github.com/gabrielmoris/writersbook",
+            name: "WritersBook",
+        },
+        {
+            id: 2,
+            img: "./assets/workPics/imageboard.png",
+            url: "https://gabrielimageboard.herokuapp.com/",
+            github: "https://github.com/gabrielmoris/imageboard",
+            name: "My Imageboard",
+        },
+        {
+            id: 3,
+            img: "./assets/workPics/signaturesbank.png",
+            url: "https://signaturebank.herokuapp.com/",
+            github: "https://github.com/gabrielmoris/signaturesBank",
+            name: "Signatures Bank",
+        },
+        {
+            id: 4,
+            img: "./assets/workPics/manu.jpg",
+            url: "https://www.manuelcasares.com",
+            github: "https://github.com/gabrielmoris/Manuel-Casares",
+            name: "Manuel Casares Website",
+        },
+        {
+            id: 5,
+            img: "./assets/workPics/spotifyScren.jpg",
+            url: "https://competent-mestorf-e41da5.netlify.app/",
+            github: "https://github.com/gabrielmoris/codingChallenges/tree/master/week4/handlebarsSpotify",
+            name: "Spotify API",
+        },
+        {
+            id: 6,
+            img: "./assets/workPics/brasscollective.png",
+            url: "https://www.thebrasscollective.com/",
+            github: "https://github.com/gabrielmoris/www.thebrasscollective.com",
+            name: "The Brass Collective",
+        },
+    ];
 
     return (
         <div className="works" id="projects">
@@ -26,13 +55,9 @@ export default function Works() {
                 <h2>Check my projects</h2>
             </div>
             <div className="bottom">
-                {works &&
-                    works.map(
-                        (work: {
-                            id: number;
-                            img: string;
-                            name: string;
-                        }) => {
+                {works2 &&
+                    works2.map(
+                        (work: { id: number; img: string; name: string }) => {
                             return (
                                 <WorksList
                                     id={work.id}
@@ -43,9 +68,9 @@ export default function Works() {
                         }
                     )}
             </div>
-                <a className="arrow-portfolio" href="#aboutme">
-                    <Down />
-                </a>
+            <a className="arrow-portfolio" href="#aboutme">
+                <Down />
+            </a>
         </div>
     );
 }
